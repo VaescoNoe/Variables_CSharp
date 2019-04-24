dc5d97322972250fa6014a6039f290c3ddfed56a
Noe <VaescoNoe@gmail.com>
2019-04-23 23:07:25 -0500

    Tipos de datos, variables

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+
+class Program
+    {
+        static void Main(string[] args)
+        {
+
+        int entero = 2;
+        Console.WriteLine(entero);
+
+        long _entero = -23232;
+        Console.WriteLine(_entero);
+
+        float _float = 2323.23f;
+        Console.WriteLine(_float);
+
+        double _double = 5435.3453452342342523;
+        Console.WriteLine(_double);
+
+        decimal _decimal = 123123123123.1231231231231231231m;
+        Console.WriteLine(_decimal);
+
+        string _string = "ASD$%W::AS";
+        Console.WriteLine(_string);
+
+        char _char = 'N';
+        Console.WriteLine(_char);
+
+        bool _bool = true;
+        Console.WriteLine(_bool);
+
+        }
+    }
+
